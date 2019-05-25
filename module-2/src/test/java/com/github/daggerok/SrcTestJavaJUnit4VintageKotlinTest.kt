b5e163b42364c8f8f1533bb9be840ee80bf4d959
Maksim Kostromin <daggerok@gmail.com>
2019-05-26 00:03:08 +0300

    Add worked parent pom.xml configuration for testing in a multi-module project.

---
+++ module-2/src/test/java/com/github/daggerok/SrcTestJavaJUnit4VintageKotlinTest.kt
@@ -0,0 +1,14 @@
+package com.github.daggerok
+
+import org.junit.Test
+import java.util.logging.Logger
+
+class SrcTestJavaJUnit4VintageKotlinTest {
+  @Test fun `test junit 4 vintage kotlin test class execution from java test sources location`() {
+    Logger.getLogger(javaClass.name).info(
+        """
+          |junit 4 vintage kotlin test has been executed from src/test/java sources folder!
+          |""".trimMargin()
+    )
+  }
+}
---
+++ module-2/src/test/java/com/github/daggerok/SrcTestJavaJUnit5JupiterJavaTest.java
@@ -0,0 +1,12 @@
+package com.github.daggerok;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.logging.Logger;
+
+class SrcTestJavaJUnit5JupiterJavaTest {
+  @Test void test_junit_5_jupiter_java_test_class_in_src_test_java_sources_execution() {
+    Logger.getLogger(getClass().getName())
+          .info("junit 5 jupiter java test has been executed from src/test/java sources folder!\n");
+  }
+}
