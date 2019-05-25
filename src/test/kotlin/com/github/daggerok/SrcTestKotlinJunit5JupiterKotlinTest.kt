2e205b94cd3dea0cc028019cdba5897b29e6cb2b
Maksim Kostromin <daggerok@gmail.com>
2019-05-25 21:28:58 +0300

    Initial commit

---
+++ src/test/kotlin/com/github/daggerok/SrcTestKotlinJunit5JupiterKotlinTest.kt
@@ -0,0 +1,14 @@
+package com.github.daggerok
+
+import org.junit.jupiter.api.Test
+import java.util.logging.Logger
+
+internal class SrcTestKotlinJunit5JupiterKotlinTest {
+  @Test internal fun `test junit 5 jupiter kotlin test class execution from kotlin test sources location`() {
+    Logger.getLogger(javaClass.name).info(
+        """
+          |junit 5 jupiter kotlin test has been executed from src/test/kotlin sources folder!
+          |""".trimMargin()
+    )
+  }
+}
